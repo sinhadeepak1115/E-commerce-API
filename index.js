f73253d473d7294a807880121eb3098a72a51d14
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-09-18 02:23:20 +0530

    basic code for jwt

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jasonwebtoken = require("jsonwebtoken");
+
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Registration failed" });
+  }
+});
